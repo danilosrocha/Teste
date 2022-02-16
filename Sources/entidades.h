98d355689c800211ec4524c902cd66111b3d07ee
Danilo Rocha <danilosilvasrocha@gmail.com>
2022-02-16 00:55:31 -0300

    Criação do teste de entidade Sessao

--- Sources/entidades.h
+++ Sources/entidades.h
@@ -6,6 +6,30 @@
 
 using namespace std;
 
+/* -----------------------------------------------------------------------
+// Estrutura de código sugerida para declaração de classe entidade.
+// Substituir Entidade por nome da classe.
+// Substituir Dominio.
+// Substituir nomeAtributo.
+
+class Entidade {
+    private:
+            Dominio nomeAtributo;                   // Declarar cada atributo da classe.
+    public:
+            void setnomeAtributo(const Dominio&);   // Declarar método set para cada atributo.
+            Dominio getnomeAtributo() const;        // Declarar método get para cada atributo.
+};
+
+inline void Entidade::setnomeAtributo(const Dominio &nomeAtributo){
+    this->nomeAtributo = nomeAtributo;
+}
+
+inline Dominio Entidade::getnomeAtributo() const{
+    return nomeAtributo;
+}
+
+----------------------------------------------------------------------- 
+
 class Usuario {
     private:
         Nome nome;
@@ -14,6 +38,10 @@ class Usuario {
     public:
         void setNome(Nome);
         Nome getNome() const;
+        void setEmail(Email);
+        Email getEmail() const;
+        void setSenha(Senha);
+        Senha getSenha() const;
 };
 
 inline void Usuario::setNome(Nome nome) {
@@ -24,8 +52,24 @@ inline Nome Usuario::getNome() const {
     return nome;
 }
 
+inline void Usuario::setEmail(Email email) {
+    this->email = email;
+}
+
+inline Email Usuario::getEmail() const {
+    return email;
+}
+
+inline void Usuario::setSenha(Senha senha) {
+    this->senha = senha;
+}
+
+inline Senha Usuario::getSenha() const {
+    return senha;
+}
+
 class Excursao {
-    public:
+    private:
         Codigo codigo;
         Titulo titulo;
         Nota nota;
@@ -34,24 +78,88 @@ class Excursao {
         Descricao descricao;
         Endereco endereco;
 
+    public:
+        void setCodigo(const Codigo);
+        Codigo getCodigo() const;
+        void setTitulo(const Titulo);
+        Titulo getTitulo() const;
+        void setNota(const Data);
+        Nota getNota() const;
+        void setDescricao(const Descricao);
+        Descricao getDescricao() const;
+        void setDuracao(const Duracao);
+        Duracao getDuracao() const;
+        void setDescricao(const Descricao);
+        Descricao getDescricao() const;
+        void setEndereco(const Endereco);
+        Endereco getEndereco() const;
+        
 };
 
 class Avaliacao {
-    public:
+    private:
         Codigo codigo;
         Nota nota;
         Descricao descricao;
-
-};
-
-class Sessao {
     public:
+        void setCodigo(const Codigo);
+        Codigo getCodigo() const;
+        void setNota(const Data);
+        Nota getNota() const;
+        void setDescricao(const Descricao);
+        Descricao getDescricao() const;
+};
+*/
+class Sessao {
+    private:
         Codigo codigo;
         Data data;
         Horario horario;
         Idioma idioma;
+    public:
+        void setCodigo(const Codigo);
+        Codigo getCodigo() const;
+        void setData(const Data);
+        Data getData() const;
+        void setHorario(const Horario);
+        Horario getHorario() const;
+        void setIdioma(const Idioma);
+        Idioma getIdioma() const;
 
 };
+
+inline void Sessao::setCodigo(const Codigo codigo) {
+    this->codigo = codigo;
+};
+
+inline Codigo Sessao::getCodigo() const {
+    return codigo;
+};
+
+inline void Sessao::setData(const Data data) {
+    this->data = data;
+};
+
+inline Data Sessao::getData() const {
+    return data;
+};
+
+inline void Sessao::setHorario(const Horario horario) {
+    this->horario = horario;
+};
+
+inline Horario Sessao::getHorario() const {
+    return horario;
+};
+
+inline void Sessao::setIdioma(const Idioma idioma) {
+    this->idioma = idioma;
+};
+
+inline Idioma Sessao::getIdioma() const {
+    return idioma;
+};
+
 
 #endif
 
--- Sources/testese.h
+++ Sources/testese.h
@@ -8,4 +8,21 @@
 
 using namespace std;
 
+class TUSessao {
+private:
+    const string VALOR_VALIDO_CODIGO = "123456";
+    const string VALOR_VALIDO_DATA = "30-Abr-2001";
+    const string VALOR_VALIDO_HORARIO = "23:30";
+    const string VALOR_VALIDO_IDIOMA = "Português";
+    Sessao *sessao;                       
+    int estado;                             
+    void setUp();                           
+    void tearDown();                        
+    void testarCenarioSucesso();            
+public:
+    const static int SUCESSO =  0;          
+    const static int FALHA   = -1;          
+    int run();                              
+};
+
 #endif // TESTES_H_INCLUDED
